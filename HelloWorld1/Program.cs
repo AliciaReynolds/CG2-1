cad1c9d5e552e4d46621731ad55e645cc980df23
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-19 22:01:03 -0500

    Initial attempt

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Type Your First Name");
+            string x = Console.ReadLine();
+            Console.WriteLine("Please Type Your Last Name");
+            string y = Console.ReadLine();
+            Console.WriteLine("Hello" + x + " " + y + "!");
+            Console.ReadLine();
+
         }
     }
 }
